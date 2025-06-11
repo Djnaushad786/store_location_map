20f079b77496e17610f0c775cde66b9915b3dfa5
naushad <naushad02012002@gmail.com>
2025-06-11 14:08:45 +0530

    change some code

--- lib/viewmodels/storeview_model.dart
+++ lib/viewmodels/storeview_model.dart
@@ -1,27 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:store_location_map/models/store_model.dart';
 import 'package:store_location_map/services/store_service.dart';
 
-class StoreViewModel with ChangeNotifier{
-
+class StoreViewModel with ChangeNotifier {
   final StoreService _storeService = StoreService();
 
-   List<StoreModel> _store = [];
-   bool _isLoading = false;
+  List<StoreModel> _store = [];
+  bool _isLoading = false;
+  StoreModel? _selectedStore;
 
-   List<StoreModel> get stores => _store;
-   bool get isLoading => _isLoading;
+  List<StoreModel> get stores => _store;
+  bool get isLoading => _isLoading;
+  StoreModel? get selectedStore => _selectedStore;
 
-   Future<void> loadStore()async{
+  Future<void> loadStore() async {
+    _isLoading = true;
+    notifyListeners();
 
-     _isLoading = true;
-     notifyListeners();
+    _store = await _storeService.fetchStoreLocation();
 
-     _store = await _storeService.fetchStoreLocation();
+    _isLoading = false;
+    notifyListeners();
+  }
 
-     _isLoading = false;
-     notifyListeners();
-
-   }
-
+  void selectStore(StoreModel store) {
+    _selectedStore = store;
+    notifyListeners();
+  }
 }
